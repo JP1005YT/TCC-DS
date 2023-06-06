1d83de782ada4a6e7f3379d19255addfbada0cf3
JP1005YT <godlolpro32@gmail.com>
2023-06-05 23:29:10 -0300

    Arrumado

--- src/index.js
+++ src/index.js
@@ -1,35 +1,43 @@
+// PACOTES
 const express = require("express")
 const cors = require("cors")
 const fs = require("fs")
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const uid = require("uuid")
-const path = require("path")
 const session = require("express-session")
 
+
+// Inicia o Servidor express
 const app = express();
 
+// Fala que vai ter sessoes
 app.use(session({
     secret: "teste",
     saveUninitialized: false,
     resave:false
 }))
 
+// Presets do app
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  });
 app.use(express.static('public'));
-
 app.use(cors());
 
+// Porta do servidor
 const PORT = 3333;
 
-
+// Função Cadastrars
 app.post('/cadastrar',async function (req, res){
     let id = res.json(await Cadastrar(req.body))
     req.session.valor = id
 });
-
+// Função Logar
 app.post('/login', async function(req, res) {
     let bdusuarios = JSON.parse(fs.readFileSync('./data/users.json'))
     let dadoslogin = req.body
@@ -50,27 +58,42 @@ app.post('/login', async function(req, res) {
 }
 
 if (encontrado) {
-    console.log(user.id)
+    
     req.session.valor = user.id
-    res.send(user)
+    
+    res.send({"res" : true})
 } else {
-    res.send("Usuário não encontrado ou senha incorreta.")
+    res.send({"res" : false})
 }
 })
-
-app.post('/alguemlogado',async function(req,res){
-    const meuValor = req.session.valor || "Valor não definido."
-    res.send(req.session.valor)
+// Função Checar alguem logado
+app.post('/check',async function(req,res){
     if(!req.session.valor){
-        console.log("Ninguem Logado")
+        res.send(false)
+    }else{
+        let bdusuarios = JSON.parse(fs.readFileSync('./data/users.json'))
+        bdusuarios.users.forEach(element => {
+            if(element.id == req.session.valor){
+                res.send(element)
+            }
+        });
     }
-    console.log(req.session.valor)
 })
-
+// Deconecta o usuario
+app.post('/sair',async function(req, res) {
+    try {
+        req.session.destroy()
+        res.send(true)
+    } catch (error) {
+        res.send(error)
+    }
+})
+// Abri o server
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
 
+// Funções Gerais
 async function gerarHash(senha) {
     const saltRounds = 10;
     const hash = await bcrypt.hash(senha, saltRounds);
@@ -93,5 +116,6 @@ async function Cadastrar(NewUser) {
 
     return NewUser.id
 }
+
 // Quando for QUERY(variavel na url "?aa=teste") req.query
 // Quando for variavel de local id/:id e para ler req.params.id
