d0c4d65a97e0ecee8008a473a6a8b7ec521a829a
Dev_ZIKI <60852195+KiraZiki@users.noreply.github.com>
2023-09-28 06:42:57 -0300

    Tcc 95%

---
+++ pages/doencas/main.js
@@ -0,0 +1,45 @@
+let u_infos
+
+function Troca_Pagina(e){
+    if(u_infos && e == "acess"){
+        window.location.href = `../../pages/profile/`
+    }else if(e == "acess"){
+        window.location.href = `../../pages/${e}/?pag=calculadora`
+    }else{
+        window.location.href = `../../pages/${e}/`
+    }
+}
+
+async function Query_Alguem_Logado(json){
+    const dados = await fetch('../../check',{
+        method: "POST",
+        body: JSON.stringify(json),
+        headers: {
+            "token": localStorage.getItem("token")
+        }
+    });
+    resposta = await dados.json();
+    console.log(resposta)
+    u_infos = resposta
+}
+
+async function Carregar_Foto(){
+    esperar = await Query_Alguem_Logado()
+    if(u_infos.profile_photo){
+            document.querySelector('.img_profile').setAttribute('src',`../../profile_images/${u_infos.profile_photo}`)
+            document.querySelector('#text-entry').style.display = 'none'
+            document.querySelector('#icon_remove').style.display = 'none'
+        }else if(u_infos){
+            document.querySelector('.img_profile').setAttribute('src',`../../resources/assets/defaut.png`)
+            document.querySelector('#icon_remove').style.display = 'none'
+            document.querySelector('#text-entry').style.display = 'none'
+        }else{
+            document.querySelector('.img_profile').style.display = 'none'
+        }
+}
+
+document.querySelector("#logohere").addEventListener("click",function(){
+    window.location.href = "../../"
+})
+
+Carregar_Foto()
